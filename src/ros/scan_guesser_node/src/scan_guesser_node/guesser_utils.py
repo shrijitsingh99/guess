3ababc4622b3095825009c2b389049f4ded5b530
Francesco Riccio <riccio.fran@gmail.com>
2019-01-24 17:46:18 +0100

    formating
    Former-commit-id: 0816c0246e7543e0d5ec3e911684c16a13aabeb2 [formerly 6ea64ae99d0c329f4821ed12f708a5d1bb7d50a6] [formerly e2bf6ea1b763fb454755668d8236dcff284416ea [formerly 241127a538e8b9cd8732e978a504c328c04f262e]]
    Former-commit-id: 794366633603ca9bca12924280cb1eae6c15353d [formerly 4e64c6517278f2e340300cab64d26188482bf3f7]
    Former-commit-id: 3b7bab3afb82a581faa2ae1b246a614f0c24064f

--- src/ros/scan_guesser_node/src/scan_guesser_node/guesser_utils.py
+++ src/ros/scan_guesser_node/src/scan_guesser_node/guesser_utils.py
@@ -3,53 +3,53 @@ import socket
 import numpy as np
 
 def println(name):
-        print name
+    print name
 
 __all__ = ['Provider', 'Receiver']
 
 
 class Provider:
-        def __init__(self, data_length, dip="127.0.0.1", dport=9559):
-                self.data_length = data_length
-                self.dip = dip
-                self.dport = int(dport)
-                self.socket = None
+    def __init__(self, data_length, dip="127.0.0.1", dport=9559):
+        self.data_length = data_length
+        self.dip = dip
+        self.dport = int(dport)
+        self.socket = None
 
-        def send(self, data):
-                self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                self.socket.connect((self.dip, self.dport))
+    def send(self, data):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.connect((self.dip, self.dport))
 
-                assert len(data.shape) == 1, \
-                        'Incorrect data shape. Should be Nx1 array'
-                assert data.shape[0] == self.data_length , \
-                        'Invalid data length. Should be (self.data_length, 1)'
+        assert len(data.shape) == 1, \
+            'Incorrect data shape. Should be Nx1 array'
+        assert data.shape[0] == self.data_length , \
+            'Invalid data length. Should be (self.data_length, 1)'
 
-                srz_data = np.array(data).tostring()
-                self.socket.sendall(srz_data)
-                self.socket.close()
+        srz_data = np.array(data).tostring()
+        self.socket.sendall(srz_data)
+        self.socket.close()
 
 class Receiver:
-        def __init__(self, data_length, dip="127.0.0.1", dport=9559):
-                self.data_length = data_length
-                self.dip = dip
-                self.dport = int(dport)
-                self.socket = None
+    def __init__(self, data_length, dip="127.0.0.1", dport=9559):
+        self.data_length = data_length
+        self.dip = dip
+        self.dport = int(dport)
+        self.socket = None
 
-        def getData(self):
-                self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                self.socket.bind((self.dip, self.dport))
-                self.socket.listen(1)
-                c, _ = self.socket.accept()
+    def getData(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind((self.dip, self.dport))
+        self.socket.listen(1)
+        c, _ = self.socket.accept()
 
-                data = b''
-                while True:
-                        block = c.recv(self.data_length*2)
-                        if not block: break
-                        data += block
-                        c.close()
-                        self.socket.close()
+        data = b''
+        while True:
+            block = c.recv(self.data_length*2)
+            if not block: break
+            data += block
+            c.close()
+            self.socket.close()
 
-                        dsrz_data = np.frombuffer(data, dtype=np.int16)
-                        return np.array(dsrz_data, dtype=np.float32)
+            dsrz_data = np.frombuffer(data, dtype=np.int16)
+            return np.array(dsrz_data, dtype=np.float32)
