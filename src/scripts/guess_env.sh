470b1a54291707604a1597c5528f9ec18205b5d1
Shrijit Singh <shrijitsingh99@gmail.com>
2021-02-08 10:53:48 +0530

    Update run scripts for new arguments

--- src/scripts/guess_env.sh
+++ src/scripts/guess_env.sh
@@ -18,9 +18,7 @@ fi
 
 GUESS_ROOT=/home/sapienzbot/ws/guess
 rcfg=${GUESS_ROOT}/config/rviz/stage_guess.config.rviz
-map_file=${GUESS_ROOT}/config/world/${map_name}
-# urdf_file=${GUESS_ROOT}/config/ros/urdf/
 cmd_vel_topic=/cmd_vel
 
 echo -e "Launching Guess-stage environment"
-xterm -hold -e "roslaunch scan_guesser_node guess_stage_env.launch worldfile:=${map_file}.world mapfile:=${map_file}.yaml urdffile:=${urdffile} cmd_vel_topic:=${cmd_vel_topic} rviz_cfg:=${rcfg}" &
+eval "roslaunch scan_guesser_node guess_stage_env.launch worldfile:=${map_file} map_name:=${map_name} cmd_vel_topic:=${cmd_vel_topic} rviz_cfg:=${rcfg}" 
